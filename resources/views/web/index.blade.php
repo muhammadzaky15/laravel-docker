e3572b24243c083ba9246e993425b512de13142e
muhammad.zaky.pnl@gmail.com <muhammad.zaky.pnl@gmail.com>
2020-01-28 20:43:45 +0700

    update dockerfile and add bootstrap

---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,41 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <script src="js/jquery-3.4.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <!-- ROW HEADER START -->
+      <div class="row">
+        <div class="col-lg-3 col-md-12 col-sm-12">
+          WILLIS KUSUMA ARCHITECTS
+        </div>
+        <div class="col-lg-9 col-md-12 col-sm-12">
+          WORKS
+        </div>
+      </div>
+      <!-- ROW HEADER END -->
+
+      <div class="row">
+        <div class="col-lg-3 col-md-12 col-sm-12">
+          <div class="span1">
+            <a href="#" class="primary-menu" data-toggle="collapse" data-target="#child-menu">
+              WORKS
+            </a>
+          </div>
+          <a href="#" id="child-menu" class="collapse" data-toggle="collapse" data-target="#childes-menu"> 
+            Here comes the text...
+          </a>
+          <div id="childes-menu" class="collapse">
+            apaaam
+          </div>
+        </div>
+        <div class="col-lg-9 col-md-12 col-sm-12">
+          WORKS
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
